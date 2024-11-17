d037a2d790d64d350d5fc2cedadb382cb2c5769d
b0szazal <b0szazal@vasvari.org>
2024-11-17 09:46:53 +0100

    created MalenkijMelo.Mobil

---
+++ MalenkijMelo/MalenkijMelo.Mobil/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MalenkijMelo.Mobil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MalenkijMelo/MalenkijMelo.Mobil/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MalenkijMelo.Mobil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
